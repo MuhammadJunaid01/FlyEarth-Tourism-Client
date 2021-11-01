823efc438c38f2bccd5d728c504d0622c4f02499
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2021-11-01 01:02:50 -0700

    added about  page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "./about.css";
+import aboutimage from "../../images/aboutus.png";
+import { Container, Row, Col } from "react-bootstrap";
 const About = () => {
   return (
     <div>
       <h1>Hello about</h1>
+      <Container>
+        <Row>
+          <Col lg={6} md={6} sm={12}>
+            <img className="img-fluid" src={aboutimage} alt="" />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
